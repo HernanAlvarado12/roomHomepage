3cd2515ba00128a7440aa1f629aeecd69f0bb0b8
unknown <hernanvid123@gmail.com>
2023-02-07 15:09:05 -0500

    header responsive and effects

--- model.js
+++ model.js
@@ -4,6 +4,6 @@ document.addEventListener('click', event => {
         event.target.nextElementSibling.nextElementSibling.classList.add('active')
     }else if(event.target.matches('#iconClose')) {
         event.target.parentElement.parentElement.classList.remove('active')
-        return
     }
 })
+
